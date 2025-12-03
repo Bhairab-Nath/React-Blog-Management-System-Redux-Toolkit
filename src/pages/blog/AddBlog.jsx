5074edb46e18d443fb777884df595dd18a0def47
Bhairab-Nath <nathbhairab22@gmail.com>
2025-12-03 21:29:00 +0545

    Navigation added

--- src/pages/blog/AddBlog.jsx
+++ src/pages/blog/AddBlog.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import Layout from '../../components/layout/Layout'
 import Form from './components/form/Form'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { createBlog } from '../../../store/blogSlice'
+import STATUSES from '../../globals/status/statuses'
+import { useNavigate } from 'react-router-dom'
 
 const AddBlog = () => {
-
+  const {status} = useSelector((state)=>state.blog)
+  
   const dispatch = useDispatch()
-
+  const navigate = useNavigate()
+  
   const handleAddBlog = (data)=>{
     dispatch(createBlog(data))
+    
+    if(status === STATUSES.SUCCESS){
+      navigate('/')
+    }
+    else{
+      navigate('/blog/add')
+    }
   }
 
   return (
